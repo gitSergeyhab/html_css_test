8825f00bcc2d11e3ef1295a3f1fdef7e2cbe471b
Sergey <redmoloch@yandex.ru>
2021-01-21 23:39:42 +0300

    tab-slider sevices

--- js/script.js
+++ js/script.js
@@ -10,3 +10,29 @@ slide.addEventListener('click', () => {
     };
     slide.setAttribute('src', sliderImgSrc[slide_id]);
 })
+
+
+const srviceTexts = [
+    'Мы с удовольствием доставим ваш товар прямо к вашему подъезду совершенно бесплатно!<br />Ведь мы неплохо заработаем, <br />поднимая его на ваш этаж!',
+    'Если купленный у нас товар поломается или заискрит, а также в случае пожара, спровоцированного его возгоранием, вы всегда можете быть уверены в нашей гарантии. Мы обменяем сгоревший товар на новый. Дом уж восстановите как-нибудь сами.',
+    'Залезть в долговую яму стало проще! <br />Кредитные консультанты придут вам на помощь.'
+]
+
+const srviceImgSrc = ['serv_img0', 'serv_img1', 'serv_img2']
+
+const services = document.querySelectorAll('.serv-items');
+const headerServ = document.querySelector('.text_services h3')
+const textServPar = document.querySelector('.text_services p')
+const servImg = document.querySelector('.services_img')
+
+services.forEach((serv, i) => {
+    serv.addEventListener('click', () => {
+        services.forEach( s => s.classList.remove('serv-items-active') );
+        serv.classList.add('serv-items-active');
+        headerServ.textContent = serv.textContent;
+        textServPar.innerHTML = srviceTexts[i];
+        srviceImgSrc.forEach( clas => servImg.classList.remove(clas) )
+        servImg.classList.add(srviceImgSrc[i]);
+    });
+
+})
